7935db967f6f6b7fa91f67e5e71f4d58ed2c91ed
Anthony Littlejohn <ant05man@yahoo.com>
2023-08-24 17:10:26 -0400

    created input to questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,42 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [
-  "What is your project's name?",
-  "Please write a short description of your project",
-  "What kind of license should your project have?",
-  "What command should be run to install dependencies?",
-  "What command should be run to run tests?",
-  "What does the user need to know about using the repo?",
-  "What does the user need to know about contributing to the repo?"
-];
+inquirer
+  .prompt([
+  {
+  type: 'input',
+  message: "What is your project's name?",
+  name: 'projectName',
+  },
+  {
+    type: 'input',
+  message: "Please write a short description of your project",
+  name: 'description',
+  },
+  {
+    type: 'input',
+  message: "What kind of license should your project have?",
+  },
+  {
+    type: 'input',
+  message: "What command should be run to install dependencies?",
+  },
+  {
+    type: 'input',
+  message: "What command should be run to run tests?",
+  },
+  {
+    type: 'input',
+  message: "What does the user need to know about using the repo?",
+  },
+  {
+    type:'input',
+  message: "What does the user need to know about contributing to the repo?"
+  }
+]);
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
