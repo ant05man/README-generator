d46b05739226304e5e733d6a76e471982e06b7f4
Anthony Littlejohn <ant05man@yahoo.com>
2023-08-27 15:34:08 -0400

    created badges for license

--- Develop/README.md
+++ Develop/README.md
@@ -1,11 +1,12 @@
 
-  # Project Name: project 1
-  ## Description: its cool
-  ## Installation: install
-  ## Tests: test
-  ## Usage: be careful
-  ## License: Boost Software License 1.0
-  ## Contributing: pull before push
+  # Project Name: adaf
+  ## Description: adf
+  ## Installation: adsfads
+  ## Tests: fdasf
+  ## Usage: 
+  ![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)  // Add license badge here
+  This project is licensed under the [Apache License 2.0](https://opensource.org/licenses/Apache-2.0) license. // Add license section here
+  ## Contributing: adsfa
 
   ...
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,24 +1,50 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// generateMarkdown.js
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  const licenseBadges = {
+    'Apache License 2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+    'MIT License': 'https://img.shields.io/badge/License-MIT-yellow.svg',
+    'Boost Software License 1.0': 'https://img.shields.io/badge/License-Boost%201.0-lightblue.svg'
+    // Add more license badges as needed
+  };
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  const badgeURL = licenseBadges[license] || '';
+  return badgeURL ? `![License](${badgeURL})` : '';
+}
 
-// TODO: Create a function to generate markdown for README
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    'Apache License 2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'MIT License': 'https://opensource.org/licenses/MIT',
+    'Boost Software License 1.0': 'https://www.boost.org/LICENSE_1_0.txt'
+    // Add more license links as needed
+  };
+
+  return licenseLinks[license] || '';
+}
+
+function renderLicenseSection(license) {
+  const licenseLink = renderLicenseLink(license);
+
+  if (licenseLink) {
+    return `This project is licensed under the [${license}](${licenseLink}) license.`;
+  } else {
+    return '';
+  }
+}
+
 function generateMarkdown(userResponses) {
+  const licenseBadge = renderLicenseBadge(userResponses.license);
+  const licenseSection = renderLicenseSection(userResponses.license);
+
   return `
   # Project Name: ${userResponses.projectName}
   ## Description: ${userResponses.description}
   ## Installation: ${userResponses.installation}
   ## Tests: ${userResponses.tests}
   ## Usage: ${userResponses.usage}
-  ## License: ${userResponses.license}
+  ${licenseBadge}  // Add license badge here
+  ${licenseSection} // Add license section here
   ## Contributing: ${userResponses.contributing}
 
   ...
